7b339e0f03d7284350263cf716ea949b6687aae0
Sasibeeke <sasiram124@gmail.com>
2023-01-15 19:49:41 -0600

    task 13 and 14 completed

---
+++ 2023/day14/add.py
@@ -0,0 +1,6 @@
+cloud_providers = ["AWS","GCP","Azure"]
+print("List is :",cloud_providers)
+cloud_providers.append("Digital Ocean")
+print("After add new list is :",cloud_providers)
+cloud_providers.sort()
+print("After sorting list is :",cloud_providers)
